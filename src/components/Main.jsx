c7e09965462c121873a46ec8f3786ad8790b175c
Arun Shakya <arun.24331@ggnindia.dronacharya.info>
2024-07-18 13:08:41 +0530

    chore: Update Main component with improved styles and layout adjustments

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,17 +13,17 @@ const Main = () => {
   })
   return (
     <div className=' bg-[#f7f7f7] sm:mb-0  ' > 
-      <div className='flex flex-col md:flex-row justify-center items-start   sm:mx-6 '>
-        <div className=' w-full md:w-1/2  px-10   lg:px-16  py-10 '>
+      <div className='flex flex-col md:flex-row justify-start items-center   sm:mx-6 '>
+        <div className=' w-full md:w-1/2  px-10   lg:px-16  my-10 '>
         <div className=''>
           <p className='text-slate-600 font-bold text-2xl ' data-aos="fade-up">Genuine Verification Service</p> 
         <h1 className='text-5xl md:text-7xl font-[Roboto] font-bold text-[#005b96] pb-4' data-aos="fade-up">GVS Your Shield Against Fraud </h1>
         {/* <h1 className='text-5xl md:text-7xl  pb-4 font-[Roboto] font-bold drop-shadow text-[#005b96]'>Welcome to GVS</h1> */}
         {/* <p className='text-slate-600 font-bold text-2xl'>Genuine Verification service</p> */}
         </div>
-          <p className='text-sm md:text-2xl  font-[Poppins] font-semibold py-12  ' data-aos="fade-up">Ensure the authenticity of your workforce with our reliable background verification services.</p>
+          <p className='text-sm md:text-xl  font-[Poppins] font-semibold py-10  ' data-aos="fade-up">Ensure the authenticity of your workforce with our reliable background verification services.</p>
           <Link to='/contact-us' data-aos="fade-up">
-              <button className="bg-blue-600 hover:bg-blue-800 text-white  font-bold py-4 px-4  my-10 rounded-3xl text-3xl " data-aos="fade-up">
+              <button className="bg-blue-600 hover:bg-blue-800 text-white  font-bold py-4 px-4  mt-10 rounded-3xl text-3xl " data-aos="fade-up">
                Verify Now
               </button>
               </Link>
