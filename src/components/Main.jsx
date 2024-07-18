0fa2fccfa6073c9b5cba5e5dacc3b849be7df7e5
Arun Shakya <arun.24331@ggnindia.dronacharya.info>
2024-07-18 13:37:21 +0530

    chore: Update Main component with adjusted image layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,9 +13,9 @@ const Main = () => {
   })
   return (
     <div className=' bg-[#f7f7f7] sm:mb-0  ' > 
-      <div className='flex flex-col md:flex-row justify-start items-center   sm:mx-6 '>
-        <div className=' w-full md:w-1/2  px-10   lg:px-16  my-10 '>
-        <div className=''>
+      <div className='flex flex-col md:flex-row justify-center items-start sm:mx-6 mt-10 md:mt-0  '>
+        <div className=' w-full md:w-1/2  px-10   lg:px-16   '>
+        <div className=' md:pt-20'>
           <p className='text-slate-600 font-bold text-2xl ' data-aos="fade-up">Genuine Verification Service</p> 
         <h1 className='text-5xl md:text-7xl font-[Roboto] font-bold text-[#005b96] pb-4' data-aos="fade-up">GVS Your Shield Against Fraud </h1>
         {/* <h1 className='text-5xl md:text-7xl  pb-4 font-[Roboto] font-bold drop-shadow text-[#005b96]'>Welcome to GVS</h1> */}
@@ -28,12 +28,17 @@ const Main = () => {
               </button>
               </Link>
         </div>
-        <div className='w-screen md:w-1/2 flex items-start  my-auto ' data-aos="fade-up"> 
+        <div className='w-screen md:w-1/2 flex-1 items-start  my-auto ' data-aos="fade-up"> 
           {/* <img className='w-auto md:w-[700px]  h-auto  md:my-0 mb-0' src='./mainImage.png' /> */}
-          <img className='w-auto md:w-[650px] mt-0 ' src='./frame2.png' />
+          <img className='w-full md:w-[650px] mt-0 ' src='./frame2.png' />
         </div>
       </div>
     </div>
+
+  
+
+
+
   )
 }
 
